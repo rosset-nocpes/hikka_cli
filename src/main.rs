3da93f9e3af09a2776a9a8468a1d2267e053d912
Andrii Blacksmith <coderpy4@proton.me>
2024-04-11 01:03:56 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,420 @@
+use std::{
+    collections::HashMap,
+    error::Error,
+    io::{self, Write},
+    process::{exit, Command, Stdio},
+    time::Duration,
+};
+
+use dotenvy::dotenv;
+use rustyline::{error::ReadlineError, DefaultEditor};
+use serde_json::{Map, Value};
+use thirtyfour::{cookie::SameSite, prelude::*};
+use tokio::{io::AsyncBufReadExt, time::sleep};
+
+async fn search_anime(anime: &str) -> String {
+    let url = "https://api.hikka.io/anime?page=1&size=5";
+    let body_data = format!(
+        r#"{{"query": "{}", "sort": ["start_date:asc", "scored_by:desc"]}}"#,
+        anime
+    );
+
+    let client = reqwest::Client::new();
+    let response = client.post(url).body(body_data.to_owned()).send().await;
+    let data: serde_json::Value = response.unwrap().json().await.unwrap();
+    let list = data["list"].as_array().unwrap();
+
+    for (i, element) in list.iter().enumerate() {
+        println!(
+            "{}. {} ({})",
+            i + 1,
+            element["title_ua"].as_str().unwrap(),
+            element["year"]
+        );
+    }
+
+    print!("Enter option: ");
+    io::stdout().flush().unwrap();
+    let mut opt = String::new();
+    let mut reader = tokio::io::BufReader::new(tokio::io::stdin());
+    reader.read_line(&mut opt).await.unwrap();
+
+    if opt == "\n" {
+        exit(1);
+    }
+
+    let opt_alt: usize = opt.trim().parse().unwrap();
+    let res = list[opt_alt - 1]["slug"].as_str().unwrap();
+    res.to_string()
+}
+
+// TODO: Rewrite to search word in characters instead of edits
+async fn search_word_ch(word: &str) -> Result<(), Box<dyn Error>> {
+    let url = "https://api.hikka.io/edit/list?page=1&size=100";
+
+    let mut map = HashMap::new();
+    map.insert("content_type", "character");
+    map.insert("status", "accepted");
+    map.insert("slug", "");
+
+    let client = reqwest::Client::new();
+    let response = client.post(url).json(&map).send().await?;
+
+    let data: serde_json::Value = response.json().await?;
+    let pages = data["pagination"]["pages"].as_u64().unwrap();
+
+    for page in 1..=pages {
+        let url = format!("https://api.hikka.io/edit/list?page={}&size=100", page);
+
+        let mut map = HashMap::new();
+        map.insert("content_type", "character");
+        map.insert("status", "accepted");
+        map.insert("slug", "");
+
+        let client = reqwest::Client::new();
+        let response = client.post(url).json(&map).send().await?;
+
+        if !response.status().is_success() {
+            eprintln!(
+                "Error fetching data from page {}: {}",
+                page,
+                response.status()
+            );
+            continue;
+        }
+
+        let data: serde_json::Value = response.json().await?;
+        let list = data["list"].as_array().unwrap();
+
+        for element in list {
+            if !element["after"]["description_ua"].is_null() {
+                match element["after"]["description_ua"].to_string().find(word) {
+                    Some(_) => {
+                        println!(
+                            "{}: https://hikka.io/edit/{}",
+                            element["content"]["name_en"].as_str().unwrap(),
+                            element["edit_id"]
+                        )
+                    }
+                    None => continue,
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+// API Method
+// NOTE: Don't working
+#[allow(dead_code)]
+async fn trans_char_anime(slug: &str) -> Result<(), Box<dyn Error>> {
+    // Creating url for anime characters
+    let url = format!("https://api.hikka.io/anime/{}/characters", slug);
+
+    // INFO: Auth the user
+    //
+    // let mut credentials = HashMap::new();
+    // credentials.insert("email", ""); // TODO: store credentials in secure
+    // credentials.insert("password", "");
+    let client = reqwest::Client::new();
+    // client
+    //     .post("https://api.hikka.io/auth/login")
+    //     .json(&credentials)
+    //     .header("captcha", "") // TODO: add captcha
+    //     .send()
+    //     .await?;
+
+    // Create edit for every character
+    let response = reqwest::get(url.clone()).await?;
+
+    let data: serde_json::Value = response.json().await?;
+    let pages = data["pagination"]["pages"].as_u64().unwrap();
+
+    for page in 1..=pages {
+        let url_p = format!("{}?page={}&size=100", url, page);
+        let response = reqwest::get(url_p).await;
+
+        let data: serde_json::Value = response.unwrap().json().await?;
+        let list = data["list"].as_array().unwrap();
+
+        for element in list {
+            if !element["character"]["name_ua"].is_null() {
+                // Output name_en & name_ja, then wait for input of name_ua
+                // After all, create and approve edit
+                print!(
+                    "{} | {} : ",
+                    element["character"]["name_en"].as_str().unwrap(),
+                    element["character"]["name_ja"].as_str().unwrap()
+                );
+                io::stdout().flush().unwrap();
+
+                let mut name_input = String::new();
+
+                let mut reader = tokio::io::BufReader::new(tokio::io::stdin());
+                reader.read_line(&mut name_input).await?;
+
+                let mut inner_map = Map::new();
+                inner_map.insert(
+                    "name_ua".to_string(),
+                    Value::String(name_input.trim().to_string()),
+                );
+
+                let mut map = Map::new();
+                map.insert(
+                    "description".to_string(),
+                    Value::String("Переклав ім'я (via HikkaCLI)".to_string()),
+                );
+                map.insert("auto".to_string(), Value::Bool(true));
+                map.insert("after".to_string(), Value::Object(inner_map));
+
+                client
+                    .post(format!(
+                        "https://api.hikka.io/edit/character/{}",
+                        element["character"]["slug"]
+                    ))
+                    .json(&map)
+                    .header("auth", "") // TODO: store auth in secure
+                    .header("captcha", "") // TODO: add captcha
+                    .send()
+                    .await?;
+                // TODO: check for success
+            } else {
+                continue;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+// WebDriver method
+// BUG: page of character edit not always opening and crashing tool
+async fn trans_char_anime_webdriver(slug: &str) -> Result<(), Box<dyn Error>> {
+    // Creating url for anime characters
+    let url = format!("https://api.hikka.io/anime/{}/characters", slug);
+
+    let mut gecko_spawner = Command::new("geckodriver")
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .spawn()
+        .unwrap();
+
+    // INFO: Auth the user
+    // TODO: First time launch login, after store token
+    //
+    // let mut credentials = HashMap::new();
+    // credentials.insert("email", ""); // TODO: store credentials in secure
+    // credentials.insert("password", "");
+    // let client = reqwest::Client::new();
+    // client
+    //     .post("https://api.hikka.io/auth/login")
+    //     .json(&credentials)
+    //     .header("captcha", "") // TODO: add captcha
+    //     .send()
+    //     .await?;
+    let hikka_token = std::env::var("AUTH_TOKEN")?;
+
+    // TODO: make option moderator or user (to auto approve or not)
+    let mut cookie = Cookie::new("auth", hikka_token); // TODO: store token in secure
+    let mut caps = DesiredCapabilities::firefox();
+
+    // NOTE: Experimental
+    caps.add_arg("--headless")?;
+
+    let driver = WebDriver::new("http://localhost:4444", caps).await?;
+
+    // setting up cookie with auth token
+    cookie.set_domain("hikka.io");
+    cookie.set_path("/");
+    cookie.set_same_site(SameSite::Lax);
+
+    driver.goto("https://hikka.io").await?;
+    driver.add_cookie(cookie.clone()).await?;
+
+    // create edit for every character
+    let response = reqwest::get(url.clone()).await?;
+
+    let data: serde_json::Value = response.json().await?;
+    let pages = data["pagination"]["pages"].as_u64().unwrap();
+
+    for page in 1..=pages {
+        let url_p = format!("{}?page={}&size=100", url, page);
+        let response = reqwest::get(url_p).await;
+
+        let data: serde_json::Value = response.unwrap().json().await?;
+        let list = data["list"].as_array().unwrap();
+
+        for element in list {
+            if element["character"]["name_ua"].is_null() {
+                // Output name_en & name_ja, then wait for input of name_ua
+                // After all, create and approve edits
+
+                // let suggest_name = wana_kana::to_kana();
+
+                // going to character page
+                driver
+                    .goto(format!(
+                        "https://hikka.io/edit/new?slug={}&content_type=character",
+                        element["character"]["slug"].as_str().unwrap()
+                    ))
+                    .await?;
+
+                let name_en = element["character"]["name_en"].to_owned();
+                let name_ja = element["character"]["name_ja"].to_owned();
+                // let waka_kana_ua = element["character"]["name_ja"].to_string().to_romaji();
+
+                // creating interactive input & waiting for input
+                // WARN: Experimental
+                let mut rl = DefaultEditor::new()?;
+                let name_input = rl.readline(
+                    format!(
+                        "{} | {} : ",
+                        if !name_en.is_null() {
+                            name_en.as_str().unwrap()
+                        } else {
+                            "-"
+                        },
+                        if !name_ja.is_null() {
+                            name_ja.as_str().unwrap()
+
+                        // TODO: integrate DeepL and 10ten
+                        } else {
+                            "-"
+                        },
+                        // if !name_ja.is_null() {
+                        //     waka_kana_ua
+                        // } else {
+                        //     "-".to_string()
+                        // }
+                    )
+                    .as_str(),
+                );
+
+                // if user enters button <ENTER>, skip character
+                match name_input {
+                    Ok(ref line) => {
+                        if line.is_empty() {
+                            continue;
+                        }
+                    }
+                    Err(ReadlineError::Interrupted) => {
+                        gecko_spawner.kill().expect("not killed!");
+                    }
+                    _ => todo!(),
+                }
+
+                // let mut inner_map = Map::new();
+                // inner_map.insert(
+                //     "name_ua".to_string(),
+                //     Value::String(name_input.trim().to_string()),
+                // );
+
+                // let mut map = Map::new();
+                // map.insert(
+                //     "description".to_string(),
+                //     Value::String("Переклав ім'я (via HikkaCLI)".to_string()),
+                // );
+                // map.insert("auto".to_string(), Value::Bool(true));
+                // map.insert("after".to_string(), Value::Object(inner_map));
+
+                let elem_edits = driver.find(By::Tag("form")).await?;
+                let name_elem = driver
+                    .find(By::XPath("/html/body/main/div/div[1]/form/div[1]/div[1]"))
+                    .await?;
+                name_elem.click().await?;
+                let button_elem = driver
+                    .find(By::XPath(
+                        "/html/body/main/div/div[1]/form/div[1]/div[1]/div[2]/div/button[1]",
+                    ))
+                    .await?;
+                button_elem.click().await?;
+                let textname_elem = driver
+                    .find(By::XPath(
+                        "/html/body/main/div/div[1]/form/div[1]/div[1]/div[2]/div[2]/input",
+                    ))
+                    .await?;
+                textname_elem.send_keys(name_input?).await?;
+
+                let desc_elem = elem_edits.find(By::Tag("textarea")).await?;
+                desc_elem.send_keys("Переклав ім'я (via HikkaCLI)").await?;
+
+                // going into iframe of captcha
+                driver.enter_frame(0).await?;
+
+                if !driver
+                    .find_all(By::XPath("/html/body/div/div/div[1]/div/label/input"))
+                    .await?
+                    .is_empty()
+                {
+                    driver
+                        .find(By::XPath("/html/body/div/div/div[1]/div/label/input"))
+                        .await?
+                        .click()
+                        .await?;
+
+                    // TODO: make dynamic check for success
+                    sleep(Duration::from_secs_f32(0.5)).await;
+                }
+
+                driver.enter_default_frame().await?;
+
+                let send_elem = driver
+                    .find(By::XPath(
+                        "/html/body/main/div/div[1]/form/div[2]/div[2]/button[2]",
+                    ))
+                    .await?;
+                send_elem.click().await?;
+
+                // TODO: Check if edit has been approved
+            } else {
+                continue;
+            }
+        }
+    }
+
+    // after all killing geckodriver for next iteration
+    gecko_spawner.kill().expect("not killed!");
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    dotenv()?;
+
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.unwrap();
+        std::process::exit(1);
+    });
+
+    println!("HikkaCLI | Tools to interact with hikka.io");
+
+    print!("Options: \n1. Search word in desc (characters only) \n2. Translate characters from anime (WebDriver) \nEnter option: ");
+    io::stdout().flush().unwrap();
+
+    let mut reader = tokio::io::BufReader::new(tokio::io::stdin());
+    let mut option_app = String::new();
+    reader.read_line(&mut option_app).await?;
+
+    match option_app.trim() {
+        "1" => {
+            print!("Enter a word: ");
+            io::stdout().flush().unwrap();
+            let mut word = String::new();
+            reader.read_line(&mut word).await?;
+
+            search_word_ch(word.trim()).await?;
+        }
+        "2" => loop {
+            print!("Enter anime title in ua: ");
+            io::stdout().flush().unwrap();
+            let mut title = String::new();
+            reader.read_line(&mut title).await?;
+
+            trans_char_anime_webdriver(&search_anime(title.trim()).await).await?;
+        },
+        &_ => todo!(),
+    }
+
+    Ok(())
+}
---
+++ src/main_next.rs
@@ -0,0 +1,136 @@
+use std::io;
+
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    prelude::*,
+    symbols::border,
+    widgets::{block::*, *},
+};
+
+mod tui;
+
+fn main() -> io::Result<()> {
+    let mut terminal = tui::init()?;
+    let app_result = App::default().run(&mut terminal);
+    tui::restore()?;
+    app_result
+}
+
+#[derive(Debug, Default)]
+pub struct App {
+    counter: u8,
+    exit: bool,
+}
+
+impl App {
+    /// runs the application's main loop until the user quits
+    pub fn run(&mut self, terminal: &mut tui::Tui) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.render_frame(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn render_frame(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.size());
+    }
+
+    /// updates the application's state based on user input
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            // it's important to check that the event is a key press event as
+            // crossterm also emits key release and repeat events on Windows.
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            _ => {}
+        }
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Title::from(" Hikka Tools CLI ".bold());
+        let instructions = Title::from(Line::from(vec![" Quit ".into(), "<Q> ".blue().bold()]));
+        let block = Block::default()
+            .title(title.alignment(Alignment::Center))
+            .title(
+                instructions
+                    .alignment(Alignment::Center)
+                    .position(Position::Bottom),
+            )
+            .borders(Borders::ALL)
+            .border_set(border::THICK);
+
+        let counter_text = Text::from(vec![Line::from(vec![
+            "Value: ".into(),
+            self.counter.to_string().yellow(),
+        ])]);
+
+        Paragraph::new(counter_text)
+            .centered()
+            .block(block)
+            .render(area, buf);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn render() {
+        let app = App::default();
+        let mut buf = Buffer::empty(Rect::new(0, 0, 50, 4));
+
+        app.render(buf.area, &mut buf);
+
+        let mut expected = Buffer::with_lines(vec![
+            "┏━━━━━━━━━━━━━ Counter App Tutorial ━━━━━━━━━━━━━┓",
+            "┃                    Value: 0                    ┃",
+            "┃                                                ┃",
+            "┗━ Decrement <Left> Increment <Right> Quit <Q> ━━┛",
+        ]);
+        let title_style = Style::new().bold();
+        let counter_style = Style::new().yellow();
+        let key_style = Style::new().blue().bold();
+        expected.set_style(Rect::new(14, 0, 22, 1), title_style);
+        expected.set_style(Rect::new(28, 1, 1, 1), counter_style);
+        expected.set_style(Rect::new(13, 3, 6, 1), key_style);
+        expected.set_style(Rect::new(30, 3, 7, 1), key_style);
+        expected.set_style(Rect::new(43, 3, 4, 1), key_style);
+
+        // note ratatui also has an assert_buffer_eq! macro that can be used to
+        // compare buffers and display the differences in a more readable way
+        assert_eq!(buf, expected);
+    }
+
+    #[test]
+    fn handle_key_event() -> io::Result<()> {
+        let mut app = App::default();
+        app.handle_key_event(KeyCode::Right.into());
+        assert_eq!(app.counter, 1);
+
+        app.handle_key_event(KeyCode::Left.into());
+        assert_eq!(app.counter, 0);
+
+        let mut app = App::default();
+        app.handle_key_event(KeyCode::Char('q').into());
+        assert_eq!(app.exit, true);
+
+        Ok(())
+    }
+}
